455762a90835322cc101c584246c3e8861c9d2bd
Valentin THOMAS <vacythe@gmail.com>
2018-03-31 08:36:07 +0200

    Work on edit config of apache

--- init.sh
+++ init.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-mkdir -p ./data ./src
-echo "<!DOCTYPE html>" > ./src/index.php
-echo "<html>" >> ./src/index.php
-echo "<head>" >> ./src/index.php
-echo "<title>TEST</title>" >> ./src/index.php
-echo "</head>" >> ./src/index.php
-echo "<body>" >> ./src/index.php
-echo "<h1>TEST BOILERPLATE</h1>" >> ./src/index.php
-echo "</body>" >> ./src/index.php
-echo "</html>" >> ./src/index.php
-mv .doxygen ./src/.doxygen
+mkdir -p ./data ./src ./config
+cp default.conf ./config/000-default.conf
+cp doxygen ./src/.doxygen
+echo "End of config, you can now work in src."
